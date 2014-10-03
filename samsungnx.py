b799bd4e7099033e57491f92de0ac21284bb29e7
Vatazhka <vatazhka@mail.ru>
2014-10-03 18:48:25 +0200

    Added Google Analytics

--- samsungnx.py
+++ samsungnx.py
@@ -34,6 +34,17 @@ def app(environ, start_response):
     
     data = '<html><body>'
     
+    data += """<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55369806-1', 'auto');
+  ga('send', 'pageview');
+
+</script>"""
+    
     t = iLauncher()
     data += 'The current version of iLauncher is <a href="' + t.url + '">' + t.version + '</a> released on ' + t.date + '.<hr>'
     
